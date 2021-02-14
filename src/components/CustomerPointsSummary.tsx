879372ebde72a05de8b25fbb84920fbf1f0fbf2f
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2021-02-14 11:59:48 -0600

    rewards-program-calculator
    Implemented logic to compute total rewards points

--- src/components/CustomerPointsSummary.tsx
+++ src/components/CustomerPointsSummary.tsx
@@ -7,13 +7,14 @@ export const CustomerPointsSummary: React.FC<ICustomerPointsSummaryProps> = ({ c
             <RewardsTableWrapper>
             <tbody>
                 <tr>
-                <th colSpan={2} className='rewards-table-header'>Customer</th>
-                <th colSpan={2} className='rewards-table-header'>Rewards Points</th>
+                <th colSpan={1} className='rewards-table-header'>Customer</th>
+                <th colSpan={4} className='rewards-table-header'>Rewards Points</th>
                 </tr>
-                {Object.entries(customerPointsSummary).map(([ key, value ]) => (
-                <tr className="table-row">
+                {customerPointsSummary && Object.entries(customerPointsSummary).map(([ key, value ]) => (
+                <tr>
                     <RewardsTableColumnWrapper key={`tablevalue-${key}`}>{key}</RewardsTableColumnWrapper>
                     {Object.entries(value['pointsPerMonth']).map(([ key, value ]) => <RewardsTableColumnWrapper key={`tablevalue-${key}`}>{key} ({value})</RewardsTableColumnWrapper>)}
+                    <RewardsTableColumnWrapper>Total Points ({value['totalPoints']})</RewardsTableColumnWrapper>
                 </tr>
                 ))}
             </tbody>
