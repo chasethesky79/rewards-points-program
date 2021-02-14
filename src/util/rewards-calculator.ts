b9986ebdc1438405a1bad10ced28bdfe00616737
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2021-02-14 13:07:43 -0600

    rewards-program-calculator
    Did a minor refactoring in the rewards calculator file

--- src/util/rewards-calculator.ts
+++ src/util/rewards-calculator.ts
@@ -13,24 +13,22 @@ const calculatePointsFromTransactionAmount = (transactionAmount: number) => tran
 transactionAmount > 100 ? 2 * (transactionAmount - 100) + 50 : -1;
 
 const calculatePointsPerMonth = (transactions: ITransactionInfo[]): IPointsTotalPerMonth => {
-   const pointsTotalPerMonth = transactions.reduce((acc: IPointsTotalPerMonth, element: ITransactionInfo) => {
+   return transactions.reduce((acc: IPointsTotalPerMonth, element: ITransactionInfo) => {
       const { transactionAmt, transactionMonth } = element;
       const pointsForTransactionAmt = calculatePointsFromTransactionAmount(transactionAmt);
       acc[transactionMonth] = !acc[transactionMonth] ? pointsForTransactionAmt : acc[transactionMonth] + pointsForTransactionAmt;
       return acc;
    }, {})
-   return pointsTotalPerMonth;
 }
 
 const buildCustomerPointsSummary = (customerTransactionMap: ICustomerTransactionMap): ICustomerPointsSummary => {
-     const customerPointsSummary: ICustomerPointsSummary = Object.entries(customerTransactionMap).reduce((acc: ICustomerPointsSummary, entry )=> {
+     return Object.entries(customerTransactionMap).reduce((acc: ICustomerPointsSummary, entry )=> {
         const [key, value] = entry;
         const pointsPerMonth = calculatePointsPerMonth(value);
         const totalPoints: number = Object.values(pointsPerMonth).reduce((acc: number, element: number) => acc + element);
         acc[key] = { pointsPerMonth, totalPoints };
         return acc;
       }, {});
-     return customerPointsSummary;
 }
 
 export const buildRewardsProgramResult = (transactions: ITransactionRecord[]) => buildCustomerPointsSummary(groupRecordsByCustomerAndMonth(transactions));
